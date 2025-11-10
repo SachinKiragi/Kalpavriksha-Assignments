a3c73057bfa47cae601f4a289216700405b41005
SachinKiragi <sachinkiragi8@gmail.com>
2025-11-10 21:56:20 +0530

    fix: cleaned up fgets input parsing

--- VirtualFileSystem/virtualFileSystem.c
+++ VirtualFileSystem/virtualFileSystem.c
@@ -79,6 +79,7 @@ void initVirtualFileSystem(){
         char command[MAX_COMMAND_LENGTH];
 
         fgets(line, sizeof(line), stdin);
+        line[strcspn(line, "\n")] = '\0';
         sscanf(line, "%s %s", command, name);
 
         if(strcmp(command, "mkdir") == 0 || strcmp(command, "create") == 0){
